88dc6cad5b3f40289686d7bd90298adb6e5fde57
hasismaeel5 <hassan@voiceit.io>
2020-12-21 11:59:10 -0600

    renaming

--- android/app/src/main/java/com/voiceitreactnativedemo/MainActivity.java
+++ android/app/src/main/java/com/voiceitreactnativedemo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.sample;
+package com.voiceitreactnativedemo;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "sample";
+    return "VoiceitReactNativeDemo";
   }
 }
--- ios/VoiceitReactNativeDemoTests/VoiceitReactNativeDemoTests.m
+++ ios/VoiceitReactNativeDemoTests/VoiceitReactNativeDemoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface sampleTests : XCTestCase
+@interface VoiceitReactNativeDemoTests : XCTestCase
 
 @end
 
-@implementation sampleTests
+@implementation VoiceitReactNativeDemoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
